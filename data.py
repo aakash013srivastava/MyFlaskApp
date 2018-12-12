0f19c92b85f7528318731da58f6aaf992ac1d648
Aakash Srivastava <aakashsrivastava013@gmail.com>
2018-12-12 20:48:42 +0530

    Initial commit

---
+++ data.py
@@ -0,0 +1,22 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article One',
+            'body':'Text for the article One',
+            'created_at':12/12/2018
+        },
+        {
+            'id':2,
+            'title':'Article Two',
+            'body':'Text for the article Two',
+            'created_at':13/12/2018
+        },
+        {
+            'id':3,
+            'title':'Article Three',
+            'body':'Text for the article Three',
+            'created_at':14/12/2018
+        }
+    ]
+    return articles
